3b008c48dbba2ef42ff24901cbf3f8ebe517851e
Utsav Barnwal <u.barnwal36@gmail.com>
2023-05-12 23:21:46 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'starter-ionic-capacitor-tailwind-ts',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
