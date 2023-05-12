8f4f02d1e8568936eb4c8a2e4c2ca1ee77f27c31
Utsav Barnwal <u.barnwal36@gmail.com>
2023-05-13 00:24:21 +0700

    add and use tailwindcss-safe-area

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,8 @@ import { IonContent, IonPage } from "@ionic/react";
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
+    // INFO: pt-safe (from tailwindcss-safe-area) plugin adds padding to the top of the page to avoid the status bar
+    <IonPage className="pt-safe">
       {/* TODO: Uncomment for page header, or clean */}
       {/* <IonHeader>
         <IonToolbar>
